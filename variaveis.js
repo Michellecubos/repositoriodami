13ea6b7c9cad39055d550bf4a47187f765eec8b4
michelle de sousa cardoso <michellesousa508@gmail.com>
2023-07-19 21:14:29 -0300

    logica que eu apendi da aula de variaveis- cubos academy

---
+++ variaveis.js
@@ -0,0 +1,15 @@
+// uma loja de bebidas tem 500 cocas colas e 300 fanta, qauntas bebidas essa adega tem;
+
+let coca = 500;
+let fanta = 300;
+
+let resultado = (500+ 300);
+console.log (800);
+
+
+
+
+
+
+
+ 
